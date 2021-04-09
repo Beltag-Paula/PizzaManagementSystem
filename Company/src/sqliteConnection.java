78da620b836ae86b090fa54030a85da638e3cd2e
Beltag-Paula <80473757+Beltag-Paula@users.noreply.github.com>
2021-04-09 23:14:16 +0300

    Update sqliteConnection.java

--- Company/src/sqliteConnection.java
+++ Company/src/sqliteConnection.java
@@ -8,6 +8,7 @@ public class sqliteConnection {
 	public static Connection dbConnector() {
 		try {
 			Class.forName("org.sqlite.JDBC");
+			//I've added the EmployeeData.db in the img file folder. Just change the path to work ;)
 			Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Paula\\Desktop\\proiecte java\\EmployeeData.db");
 			JOptionPane.showMessageDialog(null, "Connection succesfull");
 			return conn;
